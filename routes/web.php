5bc125ae638264f1484fe35b83ca62d8bbcfecb2
TenorioCarmen <tenoriocarmen243@gmail.com>
2026-04-15 22:43:58 +0200

    feat: rutas del sistema

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,11 @@ use App\Http\Controllers\logoutController;
 use App\Http\Controllers\proveedorController;
 use App\Http\Controllers\materialController;
 use App\Http\Controllers\pedidoController;
+use App\Http\Controllers\profileController;
+use App\Http\Controllers\roleController;
+use App\Http\Controllers\userController;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -24,7 +28,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/',[homeController::class,'index'])->name('panel');
+Route::get('/', [homeController::class, 'index'])->name('panel');
 
 //Route::view('/panel','panel.index')->name('panel');
 
@@ -36,12 +40,28 @@ Route::resources([
     'compras' => compraController::class,
     'materiales' => materialController::class,
     'cotizaciones' => cotizacionController::class,
-    'pedidos' => pedidoController::class
+    'pedidos' => pedidoController::class,
+    'users' => userController::class,
+    'roles' => roleController::class,
+    'profile' => profileController::class
 ]);
 
-Route::get('/login', [loginController::class,'index'])->name('login');
-Route::post('/login', [loginController::class,'login']);
-Route::get('/logout', [logoutController::class,'logout'])->name('logout');
+Route::get('/login', [loginController::class, 'index'])->name('login');
+Route::post('/login', [loginController::class, 'login']);
+Route::get('/logout', [logoutController::class, 'logout'])->name('logout');
+
+Route::post('/cotizaciones/{id}/estado', [CotizacionController::class, 'cambiarEstado'])->name('cotizaciones.cambiarEstado');
+Route::post('pedidos/{pedidoId}/comprobante', [PedidoController::class, 'updateComprobante'])->name('pedidos.updateComprobante');
+// En web.php o api.php
+Route::post('/pedidos/{id}/estado', [PedidoController::class, 'cambiarEstado'])->name('pedidos.cambiarEstado');
+
+Route::post('/productos/costo-total-materiales', [ProductoController::class, 'calcularYGuardarCostoTotalMateriales']);
+//ruta para generar el pdf
+// Ruta para visualizar el PDF en el navegador
+Route::get('/cotizaciones/{id}/ver-pdf', [CotizacionController::class, 'generarPDF'])->name('cotizaciones.ver-pdf');
+
+// Ruta para descargar el PDF
+Route::get('/cotizaciones/{id}/descargar-pdf', [CotizacionController::class, 'generarPDF'])->name('cotizaciones.descargar-pdf')->defaults('descargar', true);
 
 Route::get('/401', function () {
     return view('pages.401');
